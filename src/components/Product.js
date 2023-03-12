398d7ccf13ca678a0ccb068b67285fb3fe35f2b8
Hasan Hafizur Rahman <hasanshanto922@gmail.com>
2023-03-13 00:09:51 +0600

    products listing

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,15 +1,17 @@
+/* eslint-disable prettier/prettier */
 /* eslint-disable react/prop-types */
 import React from 'react';
 
-const Product = () => {
+const Product = ({ product }) => {
+  console.log(product.title)
   return (
       <article className="product">
-      <img src="" alt="" />
+      <img className="product__img" src={product.image} alt="" />
       <div className="product__details">
-        <h4 className="product__title">product title</h4>
-        <p>Price: $ product price</p>
-        <p>Rating: product rating rate/5</p>
-        <p className="product__desc">Description: product.description</p>
+        <h4 className="product__title">{product.title}</h4>
+        <p style={{color: "blueviolet"}}>Price: $ {product.price}</p>
+        <p style={{color: "blueviolet"}}>Rating: {product.rating.rate}</p>
+        <p className="product__desc">Description: {product.description}</p>
         <button className="product__btn btn">Add to cart</button>
       </div>
     </article>
--- src/components/Products.js
+++ src/components/Products.js
@@ -1,1 +1,18 @@
+/* eslint-disable prettier/prettier */
 /* eslint-disable react/prop-types */
+import React from 'react';
+import Product from './Product';
+
+const Products = ({ products }) => {
+    console.log(products)
+    return (
+        <div style={{display: "grid", gridTemplateColumns: "1fr 1fr 1fr"}}>
+            {products.map(product => <Product
+                key={product.id}
+                product={product}
+            />)}
+        </div>
+    );
+};
+
+export default Products;
